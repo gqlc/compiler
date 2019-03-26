1dcac5a5fa3d7c036df13577ef2fb6f458ffdb27
Carson Derr <cakub6@gmx.com>
2019-03-26 14:05:58 -0400

    [api] Added Generator Context
    Added the embedding of values in the generator context e.g. (output io.Writer)

--- gen.go
+++ gen.go
@@ -3,6 +3,7 @@ package compiler
 import (
 	"context"
 	"github.com/gqlc/graphql/ast"
+	"io"
 )
 
 // CodeGenerator provides a simple API for creating a code generator for
@@ -14,4 +15,25 @@ type CodeGenerator interface {
 
 	// GenerateAll should handle multiple schemas.
 	GenerateAll(ctx context.Context, docs []*ast.Document, opts string) error
+}
+
+// GenCtx represents request scoped data
+// for each CodeGenerator.Generate(All) call.
+type GenCtx struct {
+	// Out is where the generator should output
+	// all generated text.
+	Out io.Writer
+}
+
+const genOut = "output"
+
+// WithOutput returns a prepared context.Context with the given
+// output source.
+func WithOutput(ctx context.Context, w io.Writer) context.Context {
+	return context.WithValue(ctx, genOut, w)
+}
+
+// Output returns the output source for the generator to use.
+func Output(ctx context.Context) io.Writer {
+	return ctx.Value(genOut).(io.Writer)
 }
