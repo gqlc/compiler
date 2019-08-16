9017c3fa6d4d81942cffaf3f651f9e1184ff7f69
Carson Derr <cakub6@gmx.com>
2019-08-16 00:55:02 -0400

    [api] added type merging API

---
+++ merge.go
@@ -0,0 +1,112 @@
+package compiler
+
+import (
+	"fmt"
+	"github.com/gqlc/graphql/ast"
+	"github.com/gqlc/graphql/token"
+)
+
+// MergeExtensions merges type extensions with their original declaration.
+func MergeExtensions(types map[string][]*ast.TypeDecl) map[string][]*ast.TypeDecl {
+	for name, decls := range types {
+		if len(decls) == 1 {
+			continue
+		}
+
+		types[name] = mergeDecls(decls)
+	}
+	return types
+}
+
+func mergeDecls(decls []*ast.TypeDecl) []*ast.TypeDecl {
+	f := func(_, _ *ast.TypeSpec) {}
+	switch decls[0].Tok {
+	case token.Token_SCHEMA:
+		f = mergeSchema
+	case token.Token_SCALAR:
+	case token.Token_TYPE:
+		f = mergeObject
+	case token.Token_INTERFACE:
+		f = mergeInterface
+	case token.Token_UNION:
+		f = mergeUnion
+	case token.Token_ENUM:
+		f = mergeEnum
+	case token.Token_INPUT:
+		f = mergeInput
+	default:
+		panic(fmt.Sprintf("type of: %s cannot be extended", decls[0].Tok))
+	}
+
+	def, ok := decls[0].Spec.(*ast.TypeDecl_TypeSpec)
+	if !ok {
+		panic("") // TODO: Should be panic?
+	}
+
+	for _, edecl := range decls[1:] {
+		ext, ok := edecl.Spec.(*ast.TypeDecl_TypeExtSpec)
+		if !ok {
+			panic("") // TODO: Should be panic?
+		}
+
+		f(def.TypeSpec, ext.TypeExtSpec.Type)
+
+		def.TypeSpec.Directives = append(def.TypeSpec.Directives, ext.TypeExtSpec.Type.Directives...)
+	}
+
+	return decls[:1]
+}
+
+func mergeSchema(def, ext *ast.TypeSpec) {
+	schema := def.Type.(*ast.TypeSpec_Schema).Schema
+	extSchema := ext.Type.(*ast.TypeSpec_Schema).Schema.RootOps
+
+	if extSchema != nil {
+		schema.RootOps.List = append(schema.RootOps.List, extSchema.List...)
+	}
+}
+
+func mergeObject(def, ext *ast.TypeSpec) {
+	obj := def.Type.(*ast.TypeSpec_Object).Object
+	extObj := ext.Type.(*ast.TypeSpec_Object).Object
+
+	obj.Interfaces = append(obj.Interfaces, extObj.Interfaces...)
+
+	if extObj.Fields != nil {
+		obj.Fields.List = append(obj.Fields.List, extObj.Fields.List...)
+	}
+}
+
+func mergeInterface(def, ext *ast.TypeSpec) {
+	inter := def.Type.(*ast.TypeSpec_Interface).Interface
+	extInter := ext.Type.(*ast.TypeSpec_Interface).Interface
+
+	if extInter.Fields != nil {
+		inter.Fields.List = append(inter.Fields.List, extInter.Fields.List...)
+	}
+}
+
+func mergeUnion(def, ext *ast.TypeSpec) {
+	union := def.Type.(*ast.TypeSpec_Union).Union
+	extUnion := ext.Type.(*ast.TypeSpec_Union).Union
+
+	union.Members = append(union.Members, extUnion.Members...)
+}
+
+func mergeEnum(def, ext *ast.TypeSpec) {
+	enum := def.Type.(*ast.TypeSpec_Enum).Enum
+	extEnum := ext.Type.(*ast.TypeSpec_Enum).Enum
+
+	if extEnum.Values != nil {
+		enum.Values.List = append(enum.Values.List, extEnum.Values.List...)
+	}
+}
+
+func mergeInput(def, ext *ast.TypeSpec) {
+	input := def.Type.(*ast.TypeSpec_Input).Input
+	extInput := ext.Type.(*ast.TypeSpec_Input).Input
+
+	if extInput.Fields != nil {
+		input.Fields.List = append(input.Fields.List, extInput.Fields.List...)
+	}
+}
---
+++ merge_test.go
@@ -0,0 +1,167 @@
+package compiler
+
+import (
+	"github.com/gqlc/graphql/ast"
+	"github.com/gqlc/graphql/parser"
+	"github.com/gqlc/graphql/token"
+	"strings"
+	"testing"
+)
+
+func TestMergeExtensions(t *testing.T) {
+	testCases := []struct {
+		Name  string
+		Input string
+		check func(t *testing.T, merged *ast.TypeSpec)
+	}{
+		{
+			Name: "Schema",
+			Input: `schema {}
+
+extend schema {
+	query: Query
+}
+
+extend schema {
+	mutation: Mutation
+}`,
+			check: func(ct *testing.T, merged *ast.TypeSpec) {
+				schema, ok := merged.Type.(*ast.TypeSpec_Schema)
+				if !ok {
+					ct.Error("expected schema type")
+					return
+				}
+
+				fields := schema.Schema.RootOps.List
+				if len(fields) != 2 {
+					ct.Fail()
+					return
+				}
+			},
+		},
+		{
+			Name: "Scalar",
+			Input: `scalar Test
+
+extend scalar Test @a`,
+			check: func(ct *testing.T, merged *ast.TypeSpec) {
+				if len(merged.Directives) != 1 {
+					ct.Fail()
+					return
+				}
+			},
+		},
+		{
+			Name: "Object",
+			Input: `type Test {}
+
+extend type Test implements One
+
+extend type Test {
+	a: A
+}`,
+			check: func(ct *testing.T, merged *ast.TypeSpec) {
+				obj := merged.Type.(*ast.TypeSpec_Object).Object
+
+				if len(obj.Interfaces) != 1 {
+					ct.Fail()
+					return
+				}
+
+				if len(obj.Fields.List) != 1 {
+					ct.Fail()
+					return
+				}
+			},
+		},
+		{
+			Name: "Interface",
+			Input: `interface Test {}
+
+extend interface Test {
+	a: A
+}`,
+			check: func(ct *testing.T, merged *ast.TypeSpec) {
+				inter := merged.Type.(*ast.TypeSpec_Interface).Interface
+
+				if len(inter.Fields.List) != 1 {
+					ct.Fail()
+					return
+				}
+			},
+		},
+		{
+			Name: "Union",
+			Input: `union Test
+
+extend union Test = A | B`,
+			check: func(ct *testing.T, merged *ast.TypeSpec) {
+				union := merged.Type.(*ast.TypeSpec_Union).Union
+
+				if len(union.Members) != 2 {
+					ct.Fail()
+					return
+				}
+			},
+		},
+		{
+			Name: "Enum",
+			Input: `enum Test {}
+
+extend enum Test {
+	A
+}`,
+			check: func(ct *testing.T, merged *ast.TypeSpec) {
+				enum := merged.Type.(*ast.TypeSpec_Enum).Enum
+
+				if len(enum.Values.List) != 1 {
+					ct.Fail()
+					return
+				}
+			},
+		},
+		{
+			Name: "Input",
+			Input: `input Test {}
+
+extend input Test {
+	a: A
+}`,
+			check: func(ct *testing.T, merged *ast.TypeSpec) {
+				input := merged.Type.(*ast.TypeSpec_Input).Input
+
+				if len(input.Fields.List) != 1 {
+					ct.Fail()
+					return
+				}
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(subT *testing.T) {
+			doc, err := parser.ParseDoc(token.NewDocSet(), testCase.Name, strings.NewReader(testCase.Input), 0)
+			if err != nil {
+				subT.Error(err)
+				return
+			}
+
+			docIR := MergeExtensions(ToIR(doc.Types))
+
+			for _, decls := range docIR {
+				if len(decls) > 1 {
+					subT.Fail()
+					return
+				}
+
+				ts, ok := decls[0].Spec.(*ast.TypeDecl_TypeSpec)
+				if !ok {
+					subT.Error("expected type spec but got ext instead")
+					return
+				}
+
+				testCase.check(subT, ts.TypeSpec)
+			}
+		})
+	}
+}
