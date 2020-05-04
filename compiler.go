a91aed04d508c8e1c47eef53cce0f339fe2a0238
Zaba505 <cakub6@gmx.com>
2020-05-04 02:28:37 -0400

    fix(ir): sort types when converting back to docs

--- compiler.go
+++ compiler.go
@@ -2,7 +2,10 @@
 package compiler
 
 import (
+	"sort"
+
 	"github.com/gqlc/graphql/ast"
+	"github.com/gqlc/graphql/token"
 )
 
 // IR is a representation used by the compiler APIs.
@@ -47,24 +50,108 @@ func ToIR(docs []*ast.Document) IR {
 			l = append(l, decl)
 			types[name] = l
 		}
+
+		sortTypes(types)
 	}
 
 	return IR(ir)
 }
 
 // FromIR converts the compiler intermediate representation
-// back to a simple slice of GraphQL type declarations.
+// back to a simple slice of GraphQL Documents.
 //
 func FromIR(ir IR) []*ast.Document {
-	docs := make([]*ast.Document, len(ir))
+	docs := make([]*ast.Document, 0, len(ir))
 
 	for doc, mdecls := range ir {
-		doc.Types = doc.Types[:]
+		docs = append(docs, doc)
+		doc.Types = doc.Types[:0]
 
+		sortTypes(mdecls)
 		for _, decls := range mdecls {
 			doc.Types = append(doc.Types, decls...)
 		}
+
+		sort.Sort(byTypeAndName{types: &doc.Types})
 	}
 
 	return docs
 }
+
+type byTypeAndName struct {
+	types *[]*ast.TypeDecl
+}
+
+type ord uint8
+
+const (
+	schema ord = iota
+	scalars
+	objects
+	interfaces
+	unions
+	enums
+	inputs
+	directives
+)
+
+func (s byTypeAndName) Less(i, j int) bool {
+	is := (*s.types)[i]
+	js := (*s.types)[j]
+
+	its := is.Spec.(*ast.TypeDecl_TypeSpec).TypeSpec
+	jts := js.Spec.(*ast.TypeDecl_TypeSpec).TypeSpec
+
+	if is.Tok == js.Tok {
+		return its.Name.Name < jts.Name.Name
+	}
+
+	var iOrd, jOrd ord
+	switch is.Tok {
+	case token.Token_SCHEMA:
+		iOrd = schema
+	case token.Token_SCALAR:
+		iOrd = scalars
+	case token.Token_TYPE:
+		iOrd = objects
+	case token.Token_INTERFACE:
+		iOrd = interfaces
+	case token.Token_UNION:
+		iOrd = unions
+	case token.Token_ENUM:
+		iOrd = enums
+	case token.Token_INPUT:
+		iOrd = inputs
+	case token.Token_DIRECTIVE:
+		iOrd = directives
+	}
+
+	switch js.Tok {
+	case token.Token_SCHEMA:
+		jOrd = schema
+	case token.Token_SCALAR:
+		jOrd = scalars
+	case token.Token_TYPE:
+		jOrd = objects
+	case token.Token_INTERFACE:
+		jOrd = interfaces
+	case token.Token_UNION:
+		jOrd = unions
+	case token.Token_ENUM:
+		jOrd = enums
+	case token.Token_INPUT:
+		jOrd = inputs
+	case token.Token_DIRECTIVE:
+		jOrd = directives
+	}
+
+	return iOrd < jOrd
+}
+
+func (s byTypeAndName) Swap(i, j int) {
+	jdecl := (*s.types)[j]
+	(*s.types)[j] = (*s.types)[i]
+	(*s.types)[i] = jdecl
+}
+
+func (s byTypeAndName) Len() int { return len(*s.types) }
